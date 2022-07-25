733a949be0e77c66cc82dc0d772fcc6796828b2e
Valxer <levanphungk@gmail.com>
2022-07-25 12:13:43 +0200

    class created

---
+++ projetfinal/src/main/java/com/thales/projetfinal/model/CommandeArticle.java
@@ -0,0 +1,53 @@
+package com.thales.projetfinal.model;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Version;
+
+@Entity
+public class CommandeArticle {
+	@EmbeddedId
+	private IdCommandeArticle id;
+	private int quantite;
+	@Version
+	private int version;
+
+	public CommandeArticle() {
+	}
+
+	public CommandeArticle(IdCommandeArticle id, int quantite) {
+		super();
+		this.id = id;
+		this.quantite = quantite;
+	}
+
+	public IdCommandeArticle getId() {
+		return id;
+	}
+
+	public void setId(IdCommandeArticle id) {
+		this.id = id;
+	}
+
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	@Override
+	public String toString() {
+		return "CommandeArticle [id=" + id + ", quantite=" + quantite + ", version=" + version + "]";
+	}
+
+}
