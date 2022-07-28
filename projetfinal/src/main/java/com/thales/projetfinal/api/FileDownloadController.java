3005430de16b9e2d0cbc91e5b1a727e3ba00a203
Valxer <levanphungk@gmail.com>
2022-07-28 11:39:43 +0200

    added some depencies to try to handle file upload

---
+++ projetfinal/src/main/java/com/thales/projetfinal/api/FileDownloadController.java
@@ -0,0 +1,42 @@
+package com.thales.projetfinal.api;
+
+import java.io.IOException;
+
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thales.projetfinal.utils.FileDownloadUtil;
+
+@RestController
+public class FileDownloadController {
+
+	@CrossOrigin(origins = "*")
+	@GetMapping("/downloadFile/{fileCode}")
+	public ResponseEntity<?> downloadFile(@PathVariable("fileCode") String fileCode) {
+		FileDownloadUtil downloadUtil = new FileDownloadUtil();
+
+		Resource resource = null;
+		try {
+			resource = downloadUtil.getFileAsResource(fileCode);
+		} catch (IOException e) {
+			return ResponseEntity.status(500).build();
+		}
+
+		if (resource == null) {
+			return new ResponseEntity<>("File not found", HttpStatus.NOT_FOUND);
+		}
+
+		String contentType = "application/octet-stream";
+		String headerValue = "attachment; filename=\"" + resource.getFilename() + "\"";
+
+		return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType))
+				.header(HttpHeaders.CONTENT_DISPOSITION, headerValue).body(resource);
+	}
+}
